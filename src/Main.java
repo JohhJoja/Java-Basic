b9b9923c943fc07830cb94ccf46c53a134eaaafb
JohhJoja <08022002dimaeliseew@gmail.com>
2025-03-03 13:24:19 +0300

    enums

--- src/Main.java
+++ src/Main.java
@@ -22,9 +22,24 @@ public class Main {
 
         generic<String> box = new generic<>();
 
-        box.setItem("banana");
+        box.setter("banana");
 
-        System.out.println(box.getItem());
+        System.out.println(box.getter());
 
+
+        enums myPlanets = enums.EARTH;
+        canILiveHere(myPlanets);
+    }
+
+    static void canILiveHere(enums planet){
+        switch (planet){
+            case MARS -> System.out.println("U can live here" + " And it's a #" + planet.number);
+            case EARTH -> System.out.println("Of course"+ " And it's a #" + planet.number);
+            case PLUTO -> System.out.println("NO!"+ " And it's a #" + planet.number);
+            case VENUS -> System.out.println("May be..."+ " And it's a #" + planet.number);
+            case SATURN -> System.out.println("Yes))))");
+            case URANUS -> System.out.println("Obviously no"+ " And it's a #" + planet.number);
+            case JUPITER -> System.out.println("nonono"+ " And it's a #" + planet.number);
+        }
     }
 }
---
+++ src/enums.java
@@ -0,0 +1,19 @@
+public enum enums {
+
+    MERCURY(1),
+    VENUS(2),
+    EARTH(3),
+    MARS(4),
+    JUPITER(5),
+    SATURN(6),
+    URANUS(7),
+    NEPTUNE(8),
+    PLUTO(9);
+
+    int number;
+
+    enums(int number){
+        this.number = number;
+    }
+
+}
